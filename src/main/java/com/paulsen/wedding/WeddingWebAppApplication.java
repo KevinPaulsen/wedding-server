c81ee064bfaebb0a026ae5872d03166ac6c4fd88
EC2 Default User <ec2-user@ip-172-31-26-106.us-west-2.compute.internal>
2024-09-18 03:28:48 +0000

    Intital Commit

---
+++ src/main/java/com/paulsen/wedding/WeddingWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.paulsen.wedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingWebAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/paulsen/wedding/WeddingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulsen.wedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
