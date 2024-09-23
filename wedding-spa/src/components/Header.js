fdc9b5db6f56b0a5666fcef51a12ca6b1bd2d976
Kevin <kevindpaulsen@gmail.com>
2024-09-23 02:05:43 -0400

    Updated date on page

--- wedding-spa/src/components/Header.js
+++ wedding-spa/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className="text-center my-5">
             <h1 className="display-4">Kevin & Olivia</h1>
-            <h2 className="display-6">09.13.24</h2>
+            <h2 className="display-6">09.13.25</h2>
             <button className="btn btn-dark mt-3">RSVP</button>
         </header>
     );
