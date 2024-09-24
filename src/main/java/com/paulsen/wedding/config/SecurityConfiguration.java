b126105eb5f18bbeb9aede2559b470050bfc5d63
Kevin <kevindpaulsen@gmail.com>
2024-09-24 18:52:28 -0400

    Added Docker support, and setup github pipeline to push

--- src/main/java/com/paulsen/wedding/config/SecurityConfiguration.java
+++ src/main/java/com/paulsen/wedding/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/actuator/health").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
