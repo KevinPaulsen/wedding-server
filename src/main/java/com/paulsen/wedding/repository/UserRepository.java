b08b398700b023f5e5821878f73230fd8b9a355d
Kevin <kevindpaulsen@gmail.com>
2024-09-25 02:03:36 -0400

    Made user repository scanable (allowing logins)

--- src/main/java/com/paulsen/wedding/repository/UserRepository.java
+++ src/main/java/com/paulsen/wedding/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.paulsen.wedding.repository;
 
 import com.paulsen.wedding.model.User;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+@EnableScan
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
