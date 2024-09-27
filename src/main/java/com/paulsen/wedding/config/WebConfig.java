361e75632f44327719dde47c9d5f395a4abba2a0
Kevin <kevindpaulsen@gmail.com>
2024-09-26 21:26:44 -0400

    Fixed some web config

--- src/main/java/com/paulsen/wedding/config/WebConfig.java
+++ src/main/java/com/paulsen/wedding/config/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     public class WebConfig implements WebMvcConfigurer {
 
         private static final String[] allowedOrigins = {
-                "*",
                 "http://localhost:3000",
                 "https://d32ajgyhym5hws.cloudfront.net",
                 "https://master.d17kqty0y5q092.amplifyapp.com"
@@ -20,6 +19,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins(allowedOrigins)
-                    .allowCredentials(true);
+                    .allowCredentials(true)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
+                    .allowedHeaders("*"); // Optionally specify allowed headers
         }
     }
