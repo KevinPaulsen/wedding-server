609fa2cca308f9f1092b81ee732ea481d28c8d77
Kevin <kevindpaulsen@gmail.com>
2024-10-05 13:39:03 -0400

    Removed Gutter from BaseLayout causing white strip on right side of the screen

--- wedding-spa/src/components/home/BaseLayout.js
+++ wedding-spa/src/components/home/BaseLayout.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 const BaseLayout = ({children}) => {
     return (
         <Container fluid className="d-flex flex-column min-vh-100 p-0" style={{backgroundColor: '#ece4da'}}>
-            <Row className=''>
+            <Row className='g-0'>
                 <Header/>
             </Row>
             <Row className="flex-grow-1 g-0">
