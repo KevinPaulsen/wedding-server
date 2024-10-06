1546a1f374c1917231d371b3b0cebfccf3399f2a
Kevin <kevindpaulsen@gmail.com>
2024-10-06 14:08:18 -0400

    Added CORS config to JWT filter, also removed @WebMVC config

--- src/main/java/com/paulsen/wedding/config/WebConfig.java
+++ src/main/java/com/paulsen/wedding/config/WebConfig.java
@@ -2,11 +2,9 @@ package com.paulsen.wedding.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     private static final String[] allowedOrigins = {
