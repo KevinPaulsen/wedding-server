2f09b0d3b1c18007cb62f951da262d666808727c
Kevin <kevindpaulsen@gmail.com>
2024-10-06 01:00:10 -0400

    Changed our story page

--- wedding-spa/src/components/main/Story.js
+++ wedding-spa/src/components/main/Story.js
@@ -7,8 +7,8 @@ const Story = () => {
         <Container className='text-center'>
             <h1 className="text-center mb-3">Our Story</h1>
             <body className='story text-center mb-3'>
-            Legend says that Kevin Loves Olivia. Some have even said that Olivia loves Kevin back
-            There can be no way to know if any of this is true.
+            Legend says that Kevin Loves Olivia. Some have even said that Olivia loves Kevin back.
+            But Kevin at least made a website for her.
             </body>
             <Image src="/assets/Story.jpg" alt="Story Image" className="max-image"/>
         </Container>
