2944bb6418abd0ec77f29e01d51516be8e8b7b3e
Kevin <kevindpaulsen@gmail.com>
2024-10-07 14:32:30 -0400

    Updated admin dashboard to show RSVP's

--- wedding-spa/src/components/admin/RsvpTable.js
+++ wedding-spa/src/components/admin/RsvpTable.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import { Table, Accordion, Card, Button } from 'react-bootstrap';
+import {Table, Accordion, Card, Spinner, Col, Row} from 'react-bootstrap';
+
 
 const url = "https://api.KevinLovesOlivia.com"
 
@@ -48,61 +49,98 @@ const RsvpTable = () => {
     }, []);
 
     return (
-        <div className="table-responsive">
-            <Table striped bordered hover responsive="sm">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>RSVP Code</th>
-                    <th>Allowed Guests</th>
-                    <th>Guests Attending</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map((item, index) => (
-                    <React.Fragment key={index}>
-                        {/* Main Row */}
-                        <tr onClick={() => toggleRowExpansion(index)}>
-                            <td>{item.primaryContact.name}</td>
-                            <td>{item.rsvpCode}</td>
-                            <td>{item.allowedGuestCount}</td>
-                            <td>{item.guestCount}</td>
+        <div>
+            {/* Centered Title */}
+            <h2 className="text-center my-4">RSVP List</h2>
+
+            <div className="">
+                {loading ? (
+                    <div className="text-center">
+                        <Spinner animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    </div>
+                ) : error ? (
+                    <div className="text-danger text-center">Error: {error}</div>
+                ) : (
+                    <Table striped bordered hover responsive="sm">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>RSVP Code</th>
+                            <th>Allowed Guests</th>
+                            <th>Guests Attending</th>
                         </tr>
-                        {/* Expansion Section */}
-                        {expandedRows[index] && (
-                            <tr>
-                                <td colSpan="4">
-                                    <Accordion defaultActiveKey="0">
-                                        <Card>
-                                            <Accordion.Collapse eventKey="0">
-                                                <Card.Body>
-                                                    <div>
-                                                        <strong>Email:</strong> {item.primaryContact.email} <br />
-                                                        <strong>Phone Number:</strong> {item.primaryContact.phoneNumber} <br />
-                                                        <strong>Address:</strong> {item.primaryContact.address}
-                                                    </div>
-                                                    <hr />
-                                                    <h6>Guests:</h6>
-                                                    {item.rsvpGuestDetails.map((guest, i) => (
-                                                        <div key={i}>
-                                                            <strong>Guest Name:</strong> {guest.name} <br />
-                                                            <strong>Food Option:</strong> {guest.foodOption} <br />
-                                                            <strong>Dietary Restrictions:</strong> {guest.dietaryRestrictions || 'None'} <br />
-                                                            <strong>Other:</strong> {guest.other || 'None'}
-                                                            {i < item.rsvpGuestDetails.length - 1 && <hr />}
-                                                        </div>
-                                                    ))}
-                                                </Card.Body>
-                                            </Accordion.Collapse>
-                                        </Card>
-                                    </Accordion>
-                                </td>
-                            </tr>
-                        )}
-                    </React.Fragment>
-                ))}
-                </tbody>
-            </Table>
+                        </thead>
+                        <tbody>
+                        {data.map((item, index) => (
+                            <React.Fragment key={index}>
+                                {/* Main Row */}
+                                <tr onClick={() => toggleRowExpansion(index)}>
+                                    <td>{item.primaryContact.name}</td>
+                                    <td>{item.rsvpCode}</td>
+                                    <td>{item.allowedGuestCount}</td>
+                                    <td>{item.guestCount}</td>
+                                </tr>
+                                {/* Expansion Section */}
+                                {expandedRows[index] && (
+                                    <tr>
+                                        <td colSpan="4">
+                                            <Accordion defaultActiveKey="0">
+                                                <Card>
+                                                    <Accordion.Collapse eventKey="0">
+                                                        <Card.Body>
+                                                            {/* Container with a Grid Layout */}
+                                                            <Row>
+                                                                {/* Primary Contact Section */}
+                                                                <Col xs={12} md={4} className="text-center mb-4">
+                                                                    <h5>Primary Contact Information</h5>
+                                                                    <div className="text-start">
+                                                                        <div>{item.primaryContact.name}</div>
+                                                                        <div>{item.primaryContact.email}</div>
+                                                                        <div>{item.primaryContact.phoneNumber}</div>
+                                                                        <div>{item.primaryContact.address}</div>
+                                                                    </div>
+                                                                </Col>
+
+                                                                {/* Guests Section */}
+                                                                <Col xs={12} md={8}>
+                                                                    <h5 className="text-center">Guests Details</h5>
+                                                                    <Table size="sm">
+                                                                        <thead>
+                                                                        <tr>
+                                                                            <th>Guest Name</th>
+                                                                            <th>Food Option</th>
+                                                                            <th>Dietary Restrictions</th>
+                                                                            <th>Other</th>
+                                                                        </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                        {item.rsvpGuestDetails.map((guest, i) => (
+                                                                            <tr key={i}>
+                                                                                <td>{guest.name}</td>
+                                                                                <td>{guest.foodOption}</td>
+                                                                                <td>{guest.dietaryRestrictions || 'None'}</td>
+                                                                                <td>{guest.other || 'None'}</td>
+                                                                            </tr>
+                                                                        ))}
+                                                                        </tbody>
+                                                                    </Table>
+                                                                </Col>
+                                                            </Row>
+                                                        </Card.Body>
+                                                    </Accordion.Collapse>
+                                                </Card>
+                                            </Accordion>
+                                        </td>
+                                    </tr>
+                                )}
+                            </React.Fragment>
+                        ))}
+                        </tbody>
+                    </Table>
+                )}
+            </div>
         </div>
     );
 };
