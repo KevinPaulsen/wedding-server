93929d462fa25e60068d008e2976aa5839326b2c
Kevin <kevindpaulsen@gmail.com>
2024-10-06 21:30:24 -0400

    Updated date text to be centered on the image

--- wedding-spa/src/components/main/MainPhoto.js
+++ wedding-spa/src/components/main/MainPhoto.js
@@ -6,7 +6,7 @@ const MainPhoto = () => {
     return (
         <Container className="position-relative p-0">
             <Image src="/assets/mainCoupleImage.jpeg" alt="Main Image" className="main-image position-absolute w-100 h-100"/>
-            <h3 className="date-text position-absolute w-100 h-75 justify-content-center align-items-center">
+            <h3 className="date-text position-absolute w-100 h-100 justify-content-center align-items-center">
                 09 . 13. 2025
             </h3>
         </Container>
