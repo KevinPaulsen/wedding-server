5ec76f75392a9c215dbd5986ace6d15fea2f87f9
Kevin <kevindpaulsen@gmail.com>
2024-10-08 17:41:51 -0400

    Made the NotFound use Container element

--- wedding-spa/src/components/NotFound.js
+++ wedding-spa/src/components/NotFound.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Container} from "react-bootstrap";
 
 function NotFound() {
     return (
-        <div className="container text-center mt-5">
+        <Container className="text-center mt-5">
             <h2>404 - Page Not Found</h2>
             <p>The page you're looking for doesn't exist.</p>
             <Link to="/" className="btn btn-primary">Go Home</Link>
-        </div>
+        </Container>
     );
 }
 
