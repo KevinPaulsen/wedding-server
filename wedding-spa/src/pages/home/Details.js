89c35f4a03b5710f6beb156b256effe6d1d916b5
Kevin <kevindpaulsen@gmail.com>
2025-02-15 13:01:04 -0500

    Added title to WeddingDetails page

--- wedding-spa/src/pages/home/Details.js
+++ wedding-spa/src/pages/home/Details.js
@@ -4,6 +4,7 @@ import Timeline from "../../components/main/timeline/Timeline";
 
 const Home = () => {
     return (<BaseLayout children={<>
+        <h1 className="text-center mb-3">Wedding Details</h1>
         <Timeline/>
     </>}/>);
 }
