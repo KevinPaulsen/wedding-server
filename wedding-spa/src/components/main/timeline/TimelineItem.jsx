72f609014729b7a9a59e1b899685c1b04e27c0c8
Kevin <kevindpaulsen@gmail.com>
2025-02-15 13:41:22 -0500

    Fixed issue with mobile

--- wedding-spa/src/components/main/timeline/TimelineItem.jsx
+++ wedding-spa/src/components/main/timeline/TimelineItem.jsx
@@ -1,6 +1,7 @@
 // TimelineItem.jsx
 import React from "react";
 import '../../../styles/Timeline.css';
+import {Container, Row} from "react-bootstrap";
 
 const TimelineItem = ({
     position,
@@ -26,29 +27,26 @@ const TimelineItem = ({
     // Determine which half arc class to use (left item => right half arc, and vice versa)
     const halfArcClass = position === "left" ? "right-half" : "left-half";
 
-    return (
-            <div className={`timeline-item ${position}`} style={{ top: topOffset }}>
+    return (<div className={`timeline-item ${position}`} style={{top: topOffset}}>
                 <div className="intersection">
                     {/* The 3 circles (bullseye) */}
-                    <div className="circle circle-1" />
-                    <div className="circle circle-2" />
-                    <div className="circle circle-3" />
+                    <div className="circle circle-1"/>
+                    <div className="circle circle-2"/>
+                    <div className="circle circle-3"/>
                     {/* The half arc that alternates sides */}
-                    <div className={`arc half ${halfArcClass}`} />
+                    <div className={`arc half ${halfArcClass}`}/>
 
                 </div>
 
                 {/* Event details container */}
                 <div className="event-content">
-                    {/* Optional: show image if provided */}
                     {image && <img src={image} alt={title} className="event-image" />}
                     <div className="event-time-title">
                         {time} {title}
                     </div>
                     <div className="event-location">{location}</div>
                 </div>
-            </div>
-    );
+            </div>);
 };
 
 export default TimelineItem;
