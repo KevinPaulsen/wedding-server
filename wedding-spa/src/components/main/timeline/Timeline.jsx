643dee71f6c57ce6d209b9660b9e571d9760a7da
Kevin <kevindpaulsen@gmail.com>
2025-02-15 11:53:07 -0500

    Added spiral to top of timeline (WIP) and made some small style changes to timeline

--- wedding-spa/src/components/main/timeline/Timeline.jsx
+++ wedding-spa/src/components/main/timeline/Timeline.jsx
@@ -15,6 +15,24 @@ const Timeline = () => {
                     className="timeline-container"
                     style={{height: containerHeight}}
             >
+        {/* The spiral, absolutely positioned at the top center */}
+        <div className="spiral-top">
+            <svg
+                    width="40"
+                    height="60"
+                    viewBox="0 0 40 60"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                        d="M 20 58 V 45 C 20 42 21 41 23 40 C 43 33 38 3 17 6 C 4 8 0 21 7 28 C 11 32 25 30 25 21 C 25 14 16 12 14 20"
+                        stroke="var(--main-dark)"
+                        strokeWidth="4"
+                        strokeLinecap="round"
+                />
+            </svg>
+        </div>
+
                 {timelineEvents.map((event, index) => (<TimelineItem
                                 key={index}
                                 position={index % 2 === 0 ? "left" : "right"}
