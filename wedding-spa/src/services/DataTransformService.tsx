13e0e6b8162f733b521658404c5820c067052d75
Kevin <kevindpaulsen@gmail.com>
2025-02-18 21:28:17 -0500

    Migrated the services tab

---
+++ wedding-spa/src/services/DataTransformService.tsx
@@ -0,0 +1,19 @@
+// DataTransformService.ts
+
+export interface GuestDetail {
+    name: string;
+    foodOption: string;
+    dietaryRestrictions: string[];
+    other: string;
+}
+
+export const transformGuestDetails = (
+    guestDetails: Array<Partial<GuestDetail>>
+): GuestDetail[] => {
+    return guestDetails.map((guest) => ({
+        name: guest.name || "",
+        foodOption: guest.foodOption || "",
+        dietaryRestrictions: guest.dietaryRestrictions || [],
+        other: guest.other || "",
+    }));
+};
