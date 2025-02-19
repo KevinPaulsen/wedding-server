5e7fd41975b9a6c1a290e3205a38d30336714c75
Kevin <kevindpaulsen@gmail.com>
2025-02-18 22:56:02 -0500

    Migrated auth to ts

---
+++ wedding-spa/src/auth/PrivateRoute.tsx
@@ -0,0 +1,46 @@
+// PrivateRoute.js
+import React, { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from './AuthContext';
+import useTokenVerification from '../hooks/useTokenVerification';
+import { Spinner } from 'react-bootstrap';
+import AdminLayout from '../components/admin/AdminLayout';
+
+interface PrivateRouteProps {
+    component: React.ReactElement;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ component }) => {
+    const authContext = useContext(AuthContext);
+    if (!authContext) {
+        throw new Error('AuthContext must be used within an AuthProvider');
+    }
+    const { authToken, logout } = authContext;
+    const navigate = useNavigate();
+    const location = useLocation();
+    const isValidToken = useTokenVerification(authToken);
+
+    // Redirect to login if the token is invalid
+    if (isValidToken === false) {
+        logout();
+        navigate('/admin/login', { replace: true, state: { from: location } });
+        return null;
+    }
+
+    // While checking the token, show a loading spinner
+    if (isValidToken === null) {
+        return (
+            <AdminLayout>
+                <div className="d-flex justify-content-center">
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
+            </AdminLayout>
+        );
+    }
+
+    return component;
+};
+
+export default PrivateRoute;
