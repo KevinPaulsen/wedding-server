8924f6ce5dbaa1c926ff6588018128dd22d871aa
Kevin <kevindpaulsen@gmail.com>
2025-02-19 03:05:18 -0500

    Fixed issue with missing orange bar on photo drag

--- wedding-spa/src/components/shared/SortableGallery/Sortable.tsx
+++ wedding-spa/src/components/shared/SortableGallery/Sortable.tsx
@@ -1,21 +1,24 @@
-// Sortable.jsx
-import React, { cloneElement } from "react";
-import { useSortable } from "@dnd-kit/sortable";
+import React, {cloneElement} from "react";
+import {useSortable} from "@dnd-kit/sortable";
 
 interface SortableProps {
   id: string;
   children: React.ReactElement;
 }
 
-const Sortable: React.FC<SortableProps> = ({ id, children }) => {
-  const { attributes, listeners, isDragging, setNodeRef } = useSortable({ id });
+export default function Sortable({ id, children }: SortableProps) {
+  const { attributes, listeners, isDragging, index, activeIndex, over, setNodeRef } = useSortable({ id });
 
   return cloneElement(children as React.ReactElement<any>, {
     ref: setNodeRef,
     "data-active": isDragging,
+    "data-position":
+        activeIndex >= 0 && over?.id === id && !isDragging
+            ? index > activeIndex
+                ? "after"
+                : "before"
+            : undefined,
     ...attributes,
     ...listeners,
   });
-};
-
-export default Sortable;
+}
