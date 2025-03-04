3fa3a67f9534eb92ebc96e1ad9afe96e82ec3c38
Kevin <kevindpaulsen@gmail.com>
2025-03-03 21:15:10 -0500

    Fixed issue where all the ui was broken

--- wedding-spa/src/pages/home/GalleryPage.tsx
+++ wedding-spa/src/pages/home/GalleryPage.tsx
@@ -6,8 +6,10 @@ import PhotoGalleryComponent from "../../components/main/PhotoGalleryComponent";
 const GalleryPage: React.FC = () => {
     return (
         <BaseLayout>
-            <h1 className="text-center mb-3">Our Photo Gallery</h1>
-            <PhotoGalleryComponent/>
+            <div>
+                <h1 className="text-center mb-3">Our Photo Gallery</h1>
+                <PhotoGalleryComponent/>
+            </div>
         </BaseLayout>
     );
 };
