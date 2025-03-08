218a0c6237c7af957c3a8a50900f727871475d73
Kevin <kevindpaulsen@gmail.com>
2025-03-07 21:51:23 -0500

    Added Venmo Buttons

--- wedding-spa/src/components/main/registry/DonateCard.tsx
+++ wedding-spa/src/components/main/registry/DonateCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Button, Typography } from '@mui/material';
+import {Box, Card, CardActions, CardContent, CardMedia, Typography} from '@mui/material';
 import CustomButton from "../../shared/CustomButton";
+import VenmoDonateButton from "./VenmoDonateButton";
 
 interface GenericCardProps {
   image: string;
@@ -8,6 +9,7 @@ interface GenericCardProps {
   description: string;
   buttonText: string;
   onButtonClick: () => void;
+  showVenmo?: boolean;
 }
 
 const DonateCard: React.FC<GenericCardProps> = ({
@@ -16,6 +18,7 @@ const DonateCard: React.FC<GenericCardProps> = ({
                                                    description,
                                                    buttonText,
                                                    onButtonClick,
+                                                  showVenmo,
                                                  }) => {
   return (
       <Card sx={{ maxWidth: 345, bgcolor: "secondary.light" }}>
@@ -34,7 +37,10 @@ const DonateCard: React.FC<GenericCardProps> = ({
           </Typography>
         </CardContent>
         <CardActions sx={{ justifyContent: "center" }}>
-          <CustomButton text={buttonText} onClick={onButtonClick} variant="dark" />
+          <Box sx={{width: '100%', display: 'flex', flexDirection: 'column', gap: 1}}>
+            <CustomButton text={buttonText} onClick={onButtonClick} variant="dark" maxWidth="100%"/>
+            {showVenmo && <VenmoDonateButton/>}
+          </Box>
         </CardActions>
       </Card>
   );
---
+++ wedding-spa/src/components/main/registry/VenmoDonateButton.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@mui/material";
+import venmo from '../../../assets/venmo.png';
+
+const VenmoDonateButton = () => {
+  return (
+      <Button
+          variant="contained"
+          color="primary"
+          href="https://venmo.com/kevin-paulsen-11"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            backgroundColor: "#008CFF", // Venmo Blue
+            "&:hover": { backgroundColor: "#357ABD" },
+            padding: "8px 16px",
+            display: "flex",
+            alignItems: "center",
+          }}
+      >
+        <img
+            src={venmo}
+            alt="Venmo Icon"
+            style={{ height: 20 }}
+        />
+      </Button>
+  );
+};
+
+export default VenmoDonateButton;
