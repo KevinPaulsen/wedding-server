54ea55574f0b12e4409ef8e28e3ed15d7c2422b1
Kevin <kevindpaulsen@gmail.com>
2025-03-22 10:45:27 -0700

    Added 'Seattle, WA' to main photo text

--- wedding-spa/src/components/main/MainPhoto.tsx
+++ wedding-spa/src/components/main/MainPhoto.tsx
@@ -5,40 +5,53 @@ import {Box, Typography} from '@mui/material';
 const MainPhoto: React.FC = () => {
   return (
       <Box sx={{position: 'relative', width: '100%', height: '600px'}}>
-        <Box
-            component="img"
-            src="/assets/mainCoupleImage.jpeg"
-            alt="Main Image"
-            sx={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              objectPosition: '50% 38%',
-              filter: 'brightness(50%)',
-            }}
-        />
-        <Typography
-            variant="h3"
-            sx={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              fontFamily: '"Baskervville", serif',
-              fontWeight: 800,
-              color: 'var(--main-light)',
-              fontSize: 'clamp(1em, 12vw + 1em, 15rem)',
-            }}
-        >
-          09 . 13 . 2025
-        </Typography>
+            <Box
+                component="img"
+                src="/assets/mainCoupleImage.jpeg"
+                alt="Main Image"
+                sx={{
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover',
+                      objectPosition: '50% 38%',
+                      filter: 'brightness(50%)',
+                }}
+            />
+            <Box sx={{
+                  position: 'absolute',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '100%',
+                  height: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  gap: 4,
+            }}>
+                  <Typography
+                      variant="h3"
+                      sx={{
+                            fontFamily: '"Baskervville", serif',
+                            color: (theme) => theme.palette.primary.contrastText,
+                            fontSize: 'clamp(1em, 12vw + 1em, 15rem)',
+                      }}
+                  >
+                        09 . 13 . 2025
+                  </Typography>
+                  <Typography
+                      variant="h3"
+                      sx={{
+                            fontFamily: '"Baskervville", serif',
+                            fontWeight: 'bold',
+                            color: (theme) => theme.palette.primary.contrastText,
+                            fontSize: 'clamp(1em, 4vw + 1em, 15rem)',
+                      }}
+                  >
+                        Seattle, WA
+                  </Typography>
+            </Box>
       </Box>
   );
 };
